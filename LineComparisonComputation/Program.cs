015b4ff1e1b1a3177f8ba561389180f9cad3aa7d
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-05 15:31:13 +0530

    [Ashwani]- Welcome message

---
+++ LineComparisonComputation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparisonComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Line Comparison Computation Program");
+        }
+    }
+}
